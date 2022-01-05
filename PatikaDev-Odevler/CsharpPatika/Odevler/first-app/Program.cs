9060d5aac13febd61de1cdf019eff936fa3b86a1
Gazi Hataş <55117177+gazihatas@users.noreply.github.com>
2022-01-06 02:40:49 +0300

    ConsoleProgramlama
    PatikaDev console-programlama bölümü Main method kısmı ödevi

---
+++ PatikaDev-Odevler/CsharpPatika/Odevler/first-app/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("İsmini gir");
+String name =  Console.ReadLine();
+
+Console.WriteLine("Soyadını gir");
+String surname = Console.ReadLine();
+
+Console.WriteLine("Name : " + name + " Surname :" + surname);
